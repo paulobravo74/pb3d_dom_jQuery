1dc6ff4c6efee43248c8067beca328cb58d2e2fc
paulobravo <p_m_c_bravo@yahoo.com>
2024-02-01 20:50:53 +0000

    testes problemas em carregar imagens

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             architect: "SM Architecture",
             local: "Fernão Ferro - Portugal",
             software: "3D Studio Max - Corona - Photoshop",
-            images: ["../img/SM_FF/01_exterior.jpg", "../img/SM_FF/02_exterior.jpg", "../img/SM_FF/03_exterior.jpg", "../img/SM_FF/04_exterior.jpg", "../img/SM_FF/05_exterior.jpg", "../img/SM_FF/06_exterior.jpg"]
+            images: ["./img/SM_FF/01_exterior.jpg", "./img/SM_FF/02_exterior.jpg", "./img/SM_FF/03_exterior.jpg", "./img/SM_FF/04_exterior.jpg", "./img/SM_FF/05_exterior.jpg", "./img/SM_FF/06_exterior.jpg"]
            
         },
         {
@@ -15,35 +15,35 @@ $(document).ready(function() {
             architect: "RA Architecture",
             local: "Azeitão - Portugal",
             software: "3D Studio Max - Corona - Photoshop",
-            images: ["img/ra_az/ra_az_01.jpg", "img/ra_az/ra_az_02.jpg", "../img/RA_Az/RA_Az_03.jpg", "../img/RA_Az/RA_Az_04.jpg", "../img/RA_Az/RA_Az_05.jpg", "../img/RA_Az/RA_Az_06.jpg"]
+            images: ["./img/RA_Az/RA_Az_01.jpg", "./img/RA_Az/RA_Az_02.jpg", "./img/RA_Az/RA_Az_03.jpg", "./img/RA_Az/RA_Az_04.jpg", "./img/RA_Az/RA_Az_05.jpg", "./img/RA_Az/RA_Az_06.jpg"]
         },
         {
             id: 2,
             architect: "SM Architecture",
             local: "Cruz de Pau - Portugal",
             software: "3D Studio Max - Vray - Photoshop",
-            images: ["../img/SM_CruzPau/SM_CruzPau_01.jpg", "../img/SM_CruzPau/SM_CruzPau_02.jpg", "../img/SM_CruzPau/SM_CruzPau_03.jpg", "../img/SM_CruzPau/SM_CruzPau_04.jpg", "../img/SM_CruzPau/SM_CruzPau_05.jpg", "../img/SM_CruzPau/SM_CruzPau_06.jpg"]
+            images: ["./img/SM_CruzPau/SM_CruzPau_01.jpg", "./img/SM_CruzPau/SM_CruzPau_02.jpg", "./img/SM_CruzPau/SM_CruzPau_03.jpg", "./img/SM_CruzPau/SM_CruzPau_04.jpg", "./img/SM_CruzPau/SM_CruzPau_05.jpg", "./img/SM_CruzPau/SM_CruzPau_06.jpg"]
         },
         {
             id: 3,
             architect: "RA Architecture",
             local: "Amadora - Portugal",
             software: "3D Studio Max - Corona - Photoshop",
-            images: ["../img/CrisAmadora/CrisAmad_01_Coz_01.jpg", "../img/CrisAmadora/CrisAmad_01_Coz_02.jpg", "../img/CrisAmadora/CrisAmad_02_Quarto_01.jpg", "../img/CrisAmadora/CrisAmad_02_Quarto_02.jpg", "../img/CrisAmadora/CrisAmad_02_Quarto_03.jpg", "../img/CrisAmadora/CrisAmad_03_Sala_01.jpg", "../img/CrisAmadora/CrisAmad_03_Sala_02.jpg", "../img/CrisAmadora/CrisAmad_03_Sala_03.jpg", "../img/CrisAmadora/CrisAmad_04_WC_01.jpg", "../img/CrisAmadora/CrisAmad_04_WC_02.jpg"]
+            images: ["./img/CrisAmadora/CrisAmad_01_Coz_01.jpg", "./img/CrisAmadora/CrisAmad_01_Coz_02.jpg", "./img/CrisAmadora/CrisAmad_02_Quarto_01.jpg", "./img/CrisAmadora/CrisAmad_02_Quarto_02.jpg", "./img/CrisAmadora/CrisAmad_02_Quarto_03.jpg", "./img/CrisAmadora/CrisAmad_03_Sala_01.jpg", "./img/CrisAmadora/CrisAmad_03_Sala_02.jpg", "./img/CrisAmadora/CrisAmad_03_Sala_03.jpg", "./img/CrisAmadora/CrisAmad_04_WC_01.jpg", "./img/CrisAmadora/CrisAmad_04_WC_02.jpg"]
         },
         {
             id: 4,
             architect: "PA Architecture",
             local: "Charneca - Portugal",
             software: "3D Studio Max - Vray - Photoshop",
-            images: ["../img/PA_Mesquita/PA_Mesq_01.jpg", "../img/PA_Mesquita/PA_Mesq_02.jpg", "../img/PA_Mesquita/PA_Mesq_03.jpg", "../img/PA_Mesquita/PA_Mesq_04.jpg", "../img/PA_Mesquita/PA_Mesq_05.jpg", "../img/PA_Mesquita/PA_Mesq_06.jpg", "../img/PA_Mesquita/PA_Mesq_07.jpg"]
+            images: ["./img/PA_Mesquita/PA_Mesq_01.jpg", "./img/PA_Mesquita/PA_Mesq_02.jpg", "./img/PA_Mesquita/PA_Mesq_03.jpg", "./img/PA_Mesquita/PA_Mesq_04.jpg", "./img/PA_Mesquita/PA_Mesq_05.jpg", "./img/PA_Mesquita/PA_Mesq_06.jpg", "./img/PA_Mesquita/PA_Mesq_07.jpg"]
         },
         {
             id: 5,
             architect: "SM Architecture",
             local: "Aroeira - Portugal",
             software: "3D Studio Max - Corona - Photoshop",
-            images: ["../img/SM_Cantarelo/SM_Cant_01_Noite_01.jpg", "../img/SM_Cantarelo/SM_Cant_01_Noite_02.jpg", "../img/SM_Cantarelo/SM_Cant_02_Dia_01.jpg", "../img/SM_Cantarelo/SM_Cant_02_Dia_02.jpg", "../img/SM_Cantarelo/SM_Cant_03_Sala_01.jpg", "../img/SM_Cantarelo/SM_Cant_03_Sala_02.jpg", "../img/SM_Cantarelo/SM_Cant_03_Sala_03.jpg", "../img/SM_Cantarelo/SM_Cant_03_Sala_04.jpg", "../img/SM_Cantarelo/SM_Cant_03_Sala_05.jpg"]
+            images: ["./img/SM_Cantarelo/SM_Cant_01_Noite_01.jpg", "./img/SM_Cantarelo/SM_Cant_01_Noite_02.jpg", "./img/SM_Cantarelo/SM_Cant_02_Dia_01.jpg", "./img/SM_Cantarelo/SM_Cant_02_Dia_02.jpg", "./img/SM_Cantarelo/SM_Cant_03_Sala_01.jpg", "./img/SM_Cantarelo/SM_Cant_03_Sala_02.jpg", "./img/SM_Cantarelo/SM_Cant_03_Sala_03.jpg", "./img/SM_Cantarelo/SM_Cant_03_Sala_04.jpg", "./img/SM_Cantarelo/SM_Cant_03_Sala_05.jpg"]
         }
     ];
 
@@ -82,7 +82,7 @@ $(document).ready(function() {
     
     let image_test = $("#teste01");
 
-    let image_test_input = '<img src="' + portfolio[1].images[0] + '" alt="" />'
+    let image_test_input = '<img src=./"' + portfolio[1].images[0] + '" alt="" />'
     $(image_test_input).appendTo(image_test);
     console.log(image_test_input);
     console.log(portfolio[1].images[0])
