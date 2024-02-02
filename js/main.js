4e166965c8c5efde1c55d28762321723bb0687b0
paulobravo <p_m_c_bravo@yahoo.com>
2024-02-02 14:10:00 +0000

    update Slides

--- js/main.js
+++ js/main.js
@@ -49,10 +49,104 @@ $(document).ready(function() {
 
 
     
+    //---------------------------------------------------//
+    //-------------------- Slides -----------------------//
+    //---------------------------------------------------//
+
+    
+    let album_slider_random_array = [];
+    let album_slider_image_random_array = [];
+    let number_Of_Slides = portfolio.length;
+    
+    
+    
+    // --------- Selection albuns ---------------
+    for (let i = 0; i < number_Of_Slides; i++) {
+        
+        const album_slider_random = Math.floor(Math.random() * portfolio.length);
+        
+        album_slider_random_array.push(album_slider_random);
+        
+        album_slider_image_random_array.push(Math.floor(Math.random() * portfolio[album_slider_random_array[i]].images.length))
+        
+        console.log("album- " + album_slider_random_array);
+        console.log("imagem- " + album_slider_image_random_array);
+        
+    }
+    
+
+
+    let album_repeat = album_slider_random_array;
+    let repeat = false
+
+    for (let i = 0; i < album_repeat.length; i++) {
+
+        let count_repeat = 0;
+
+        console.log(album_repeat[i]);
+
+
+        for (let j = 0; j < album_slider_random_array.length; j++) {
+            
+            console.log(album_slider_random_array[j]);
+            
+            if (album_repeat[i] === album_slider_random_array[j]) {
+                count_repeat++;
+                //console.log("Repetition - " + count_repeat);
+            }
+            
+        }
+        
+        
+        count_repeat > 1 ? repeat=true : repeat=false;
+        console.log(repeat); 
+            
+    }
+
+
+    let slider_img_Select = 0;
+
+    
+    let Slider_input_Img = function() {
+        
+        let slider_link_arch = portfolio[album_slider_random_array[slider_img_Select]].architect;
+        let slider_link_local = portfolio[album_slider_random_array[slider_img_Select]].local;
+        let slider_link_image = portfolio[album_slider_random_array[slider_img_Select]].images[album_slider_image_random_array[slider_img_Select]];
+
+
+        // ----------------- Images -----------------------
+        let slider_images = $("#slider_Images");
+        $(slider_images).empty();
+        let slider_image_input = '<img class="slider_img" src="' + slider_link_image + '" alt=""></img>';
+        $(slider_image_input).appendTo(slider_images);
+
+
+        // ------------------ Text -----------------------
+        let slider_Text = $("#slider_text_ID");
+        $(slider_Text).empty();
+        let slider_arch_input = '<h3 class="slider_text">' + slider_link_arch + '<br>' +
+                                slider_link_local + '</h3>'; 
+        $(slider_arch_input).appendTo(slider_Text);
+        
 
 
 
+        if (slider_img_Select < album_slider_random_array.length - 1) {
+            slider_img_Select++
+        } else {
+            slider_img_Select = 0
+        }
 
+        console.log(slider_img_Select);
+
+
+        setTimeout(Slider_input_Img, 3000);
+    }
+
+    Slider_input_Img();
+
+
+/*
     let text_array = [2, 3, 5];
 
 
@@ -81,9 +175,15 @@ $(document).ready(function() {
 
     
     let image_test = $("#teste01");
+    
 
-    let image_test_input = '<img src="' + portfolio[1].images[0] + '" alt="" />'
+    let image_test_input = '<img src="' + portfolio[1].images[0] + '" alt="" />';
+    $(image_test).className = "image";
     $(image_test_input).appendTo(image_test);
     console.log(image_test_input);
     console.log(portfolio[1].images[0])
+
+*/
+
+
 }); 
